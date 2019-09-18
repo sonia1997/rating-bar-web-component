57c0324fc0d506900717e6481ca695fba8274c8c
sonia1997 <30790990+sonia1997@users.noreply.github.com>
2019-09-18 16:48:39 +0530

    Modified

--- icon-toggle.js
+++ icon-toggle.js
@@ -1,25 +1,31 @@
 import { PolymerElement, html } from '@polymer/polymer/polymer-element.js';
-import '@polymer/iron-icon/iron-icon.js';
+// import '@polymer/iron-icon/iron-icon.js';
+import 'https://unpkg.com/tonysoft@1.53.21/iron-icons.js'
 
 class IconToggle extends PolymerElement {
   static get template() {
     return html`
-      <style>
-        :host {
-          display: inline-block;
-        }
-        iron-icon {
-          fill: var(--icon-toggle-color, rgba(0,0,0,0));
-          stroke: var(--icon-toggle-outline-color, currentcolor);
-        }
-        :host([pressed]) iron-icon {
-          fill: var(--icon-toggle-pressed-color, currentcolor);
-        }
-      </style>
+            <style>
+            :host {
+        display: inline-block;
+      }
+      iron-icon {
+        fill: var(--icon-toggle-color, rgba(0,0,0,0));
+        stroke: var(--icon-toggle-outline-color, currentcolor);
+      }
+      :host([pressed]) iron-icon {
+        fill: var(--icon-toggle-pressed-color, currentcolor);
+      }
+        </style>
+
       <!-- shadow DOM goes here -->
       <iron-icon icon="[[toggleIcon]]"></iron-icon>
+      <icon-toggle icon="add"></icon-toggle>
+      <h3>Welcome to Remix Labs</h3>
+
     `;
   }
+
   static get properties () {
     return {
       toggleIcon: {
@@ -27,12 +33,13 @@ class IconToggle extends PolymerElement {
       },
       pressed: {
         type: Boolean,
+        value: false,
         notify: true,
-        reflectToAttribute: true,
-        value: false
+        reflectToAttribute: true
       }
     };
   }
+
   constructor() {
     super();
     this.addEventListener('click', this.toggle.bind(this));
